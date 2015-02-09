a2f37736af1d6881ca0747181c09f525e9b4aaa0
MikeProteus97 <mikeandroid97@gmail.com>
2015-02-09 17:46:59 -0500

    Update PID CODE
    Saturate Integral Value

--- TB6612FNG.cpp
+++ TB6612FNG.cpp
@@ -1,109 +1,251 @@
+extern unsigned int __bss_end;
+extern unsigned int __heap_start;
+extern void *__brkval;
+
 #include "TB6612FNG.h"
+#include <stdint.h>
 
-#define NO_LINE    0
+#define NO_LINE  0
 
-TB6612FNG::TB6612FNG( byte PinsIn[], int freq_PWM)
+TB6612FNG::TB6612FNG( int PinsIn[], int freq_PWM, uint8_t Conecction)
 {
 
- for (byte i=0;i<6;i++){
+
+  this->conec=Conecction;
+
+  switch (Conecction){
+    case 2:
+
+    for (int i=0;i < 4 ;i++){
+     this->Pins[i]=PinsIn[i];
+     pinMode(this->Pins[i],OUTPUT);
+   }
+
+
+   setPwmFrequency(this->Pins[0],freq_PWM);
+   setPwmFrequency(this->Pins[3],freq_PWM);
+   if (PinsIn[0]==5 || PinsIn[3]==6 || PinsIn[0]==6 || PinsIn[3]==5)
+   {
+    num=1;
+  }else{
+    num=2;
+  }
+  break;
+  default:
+
+  for (int i=0;i < 6 ;i++){
    this->Pins[i]=PinsIn[i];
    pinMode(this->Pins[i],OUTPUT);
  }
 
- if( freq_PWM != PWM_FREQUENCY_DEFAULT ){
-   setPwmFrequency(this->Pins[0],freq_PWM);
-   setPwmFrequency(this->Pins[5],freq_PWM);
- }
+ setPwmFrequency(this->Pins[0],freq_PWM);
+ setPwmFrequency(this->Pins[5],freq_PWM);
+ if (PinsIn[0]==5 || PinsIn[5]==6 || PinsIn[0]==6 || PinsIn[5]==5)
+ {
+  num=1;
+}else{
+  num=2;
+}
+break;
+}
 
-
- rotating = true;
- direction1 = FORWARD;
- direction2 = FORWARD;
- speed1 = 255;
- speed2 = 255;
- speed_regulation = 255;
+Fast_PWM_frequency = (float)((16000000L)/ ((int)freq_PWM) / (255) / (num)); 
+rotating = true;
+direction1 = FORWARD;
+direction2 = FORWARD;
+speed1 = 255;
+speed2 = 255;
+speed_regulation=255;
+lastspeed_regulation=255;
+SpeedLine=255;
 }
 TB6612FNG::~TB6612FNG(){
-delete[] Pins;
-if (Pins)
-    free(Pins);
-}
-void TB6612FNG::Stop()
-{
+  delete[] Pins;
+  if(Pins)
+   free(Pins);
+  
 
- setOutput(this->Pins[2], 0);
- setOutput(this->Pins[1], 0);
- analogWrite(this->Pins[0], 0);
- setOutput(this->Pins[3], 0);
- setOutput(this->Pins[4], 0);
- analogWrite(this->Pins[5], 0);
- rotating = false;
+}
+void TB6612FNG::Stop(int S_Time)
+{
+  switch (conec){
+   case 2:
+   setOutput(this->Pins[0], 0);
+   setOutput(this->Pins[1], 0);
+   setOutput(this->Pins[2], 0);
+   setOutput(this->Pins[3], 0);
+   break;
+   default:
+   setOutput(this->Pins[2], 0);
+   setOutput(this->Pins[1], 0);
+   analogWrite(this->Pins[0], 0);
+   setOutput(this->Pins[3], 0);
+   setOutput(this->Pins[4], 0);
+   analogWrite(this->Pins[5], 0);
+   break;
+ }
+ if( S_Time != 0){
+  delay(S_Time);
+}
+rotating = false;
 }
 void TB6612FNG::setSpeed(int n_spd1, int n_spd2)
 {
  speed1=constrain(n_spd1,(speed_regulation*-1),speed_regulation);
  speed2=constrain(n_spd2,(speed_regulation*-1),speed_regulation);
- if( speed1 > 30 || speed2 > 30 ){
-rotating = true;
- if (rotating){
-  if(speed1 < 0) {
-    direction1=REVERSE;
+ fem1 =(speed1 > 30 | speed1 < -30);
+ fem2 =(speed2 > 30 | speed2 < -30);
+
+ if (fem1 == false )
+ {
+   speed1=0;
+ }else if (fem2 == false)
+ {
+  speed2=0;
+}
+if( (speed1 !=0) || (speed2 !=0) ){
+  rotating = true;
+  if (rotating){
+    if(speed1 < 0) {
+      direction1=REVERSE;
+    }
+    else{
+     direction1=FORWARD;
+   }
+
+   if(speed2<0) {
+    direction2=REVERSE;
   }
-  else{
-   direction1=FORWARD;
+  else {
+    direction2=FORWARD;
+  }
+  switch (conec){
+   case 2:
+   if (direction1==FORWARD){
+    if (abs(speed1) == 255)
+    {
+      setOutput(this->Pins[0],1);
+    }else{
+      analogWrite(this->Pins[0], abs(speed1));
+    }
+    setOutput(this->Pins[1], 0);
+  }else{
+   setOutput(this->Pins[0], 0);
+   if (speed1 == 255)
+   {
+    setOutput(this->Pins[1],1);
+  }else{
+    analogWrite(this->Pins[1], speed1);
+  }
+  
+}
+if(direction2==REVERSE){
+ setOutput(this->Pins[2], 0);
+ if (abs(speed2) == 255)
+ {
+  setOutput(this->Pins[3],1);
+}else{
+ analogWrite(this->Pins[3], abs(speed2));
+}
+}else{
+  setOutput(this->Pins[3], 0);
+  if(speed2 == 255)
+  {
+    setOutput(this->Pins[2],1);
+  }
+else{
+     analogWrite(this->Pins[2], speed2);
+   }
  }
-
- if(speed2<0) {
-  direction2=REVERSE;
-}
-else {direction2=FORWARD;
-}
-
-if (direction1==REVERSE){
+ break;
+ default:
+ if (direction1==FORWARD){
   setOutput(this->Pins[2], 1);
   setOutput(this->Pins[1], 0);
-  analogWrite(this->Pins[0], abs(speed1));
+  if (abs(speed1) == 255){
+    setOutput(this->Pins[0],1);
+  }else{
+   analogWrite(this->Pins[0], abs(speed1));
+ }
+
+
 }else{
  setOutput(this->Pins[2], 0);
  setOutput(this->Pins[1], 1);
- analogWrite(this->Pins[0], speed1);
+ if (speed1 == 255){
+  setOutput(this->Pins[0],1);
+}else{
+  analogWrite(this->Pins[0], speed1);
 }
-if(direction2==FORWARD){
+
+}
+if(direction2==REVERSE){
  setOutput(this->Pins[3], 1);
  setOutput(this->Pins[4], 0);
+ if (abs(speed2)== 255){
+  setOutput(this->Pins[5],1);
+}else{
  analogWrite(this->Pins[5], abs(speed2));
-
+}
 }else{
   setOutput(this->Pins[3], 0);
   setOutput(this->Pins[4], 1);
-  analogWrite(this->Pins[5], speed2);
-}
-}
-
-}else{
-  rotating=false;
-  if (speed1 <= 30)
-  {
-  setOutput(this->Pins[2], 0);
-  setOutput(this->Pins[1], 0);
-  analogWrite(this->Pins[0], 0);
-  }else if (speed2 <= 30 )
-  {
-  setOutput(this->Pins[3], 0);
-  setOutput(this->Pins[4], 0);
-  analogWrite(this->Pins[5], 0);
+  if (speed2== 255){
+    setOutput(this->Pins[5],1);
+  }else{
+    analogWrite(this->Pins[5], speed2);
   }
 }
+break;
 }
-void TB6612FNG::ChangeSpeedRegulation(int n_spd_reg)
+}
+}else{
+
+  switch (conec){
+   case 2:
+   if (speed1 < 30)
+   {
+    setOutput(this->Pins[0], 0);
+    setOutput(this->Pins[1], 0);
+  }else if (speed2 < 30 )
+  {
+    setOutput(this->Pins[2], 0);
+    setOutput(this->Pins[3], 0);
+  }
+  break;
+  default:
+  if (speed1 < 30  )
+  {
+    setOutput(this->Pins[2], 0);
+    setOutput(this->Pins[1], 0);
+    setOutput(this->Pins[0], 0);
+  }else if (speed2 < 30 )
+  {
+    setOutput(this->Pins[3], 0);
+    setOutput(this->Pins[4], 0);
+    setOutput(this->Pins[5], 0);
+  }
+  break;
+}
+rotating=false;
+}
+}
+void TB6612FNG::ChangeSpeedRegulation(int n_spd_reg,int SpeedMax)
 {
  speed_regulation = n_spd_reg;
+ lastspeed_regulation = n_spd_reg;
+ if(SpeedMax != NO_LINE){
+  SpeedLine=SpeedMax;
+} else{
+  SpeedLine=n_spd_reg;
 }
-byte TB6612FNG::GetSpeedRegulation()
+
+}
+uint8_t TB6612FNG::GetSpeedRegulation()
 {
  return speed_regulation;
 }
-void TB6612FNG::setPwmFrequency(byte pin, int divisor) {
+void TB6612FNG::setPwmFrequency(uint8_t pin, int divisor) {
 
   if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
     switch(divisor) {
@@ -116,8 +258,11 @@ void TB6612FNG::setPwmFrequency(byte pin, int divisor) {
     }
     if(pin == 5 || pin == 6) {
       TCCR0B = TCCR0B & 0b11111000 | mode;
+      TCCR0A = _BV(COM0A1) | _BV(COM0B1) | _BV(WGM01) | _BV(WGM00); 
+      TCCR0B = _BV(CS00); 
     } else {
       TCCR1B = TCCR1B & 0b11111000 | mode;
+
     }
   } else if(pin == 3 || pin == 11) {
     switch(divisor) {
@@ -131,9 +276,15 @@ void TB6612FNG::setPwmFrequency(byte pin, int divisor) {
       default: return;
     }
     TCCR2B = TCCR2B & 0b11111000 | mode;
+    TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+    TCCR2B = _BV(CS22);
   }
+
 }
-void TB6612FNG::setOutput(byte pin,boolean state){
+double TB6612FNG::GetFrequency(){
+  return Fast_PWM_frequency;
+}
+void TB6612FNG::setOutput(uint8_t pin,boolean state){
 
   if(state){
     switch(pin){
@@ -188,11 +339,12 @@ void TB6612FNG::setOutput(byte pin,boolean state){
 }
 
 void TB6612FNG::FollowerLinePID(double setPoint, 
-                                 double InputMax, 
-                                 double Kp, 
-                                 double Ki,
-                                 double Kd, 
-                                 int maxTime){
+  double InputMax, 
+  double Kp, 
+  double Ki,
+  double Kd, 
+  int maxTime){
+
   this->_Kp=Kp;
   this->_Kd=Kd;
   this->_Ki=Ki;
@@ -204,58 +356,115 @@ void TB6612FNG::FollowerLinePID(double setPoint,
   this->_maxTime=maxTime;
   followerLine=false;
   Flag=false;
+  Flag2=false;
+  
+   if (maxTime == 0)
+  {
+    cont=-1;
+  }else{
+    cont=0;
+  }
   Time = millis();
 }
 
 bool TB6612FNG::ControlPID(double input){
+
+/*
+   * Pseudo code (source Wikipedia)
+   * 
+     previous_error = 0
+     integral = 0 
+   start:
+     error = setpoint – PV [actual_position]
+     integral = integral + error*dt
+     derivative = (error - previous_error)/dt
+     output = Kp*error + Ki*integral + Kd*derivative
+     previous_error = error
+     wait(dt)
+     goto start
+   */
+ 
   input = (double)map(input, 0, this->ref, 0, 100);
-  input = (double)constrain(input, 0, 100);
-  error = (double)(input - this->_setPoint);
-  if(error <=1 && error >=-1){
-    error=0;
-  }
- motorSpeed = (double)((this->_Kp * error) + (this->_Ki*integral) + (this->_Kd * (error - lastError)));
-rightMotorSpeed = numpd((255 + motorSpeed));
-leftMotorSpeed = numpd((255 - motorSpeed));
-integral = (long)(integral + error);
+  input = constrain(input,0,100);
+  error = (double)(input - this->_setPoint );
 
-if (followerLine != true){
-  Flag=false;
-if (rightMotorSpeed > speed_regulation ) rightMotorSpeed = speed_regulation; 
+ switch((int)error) {
+       case 1:
+              error=0;
+              break;
+      case -1:
+             error=0;
+             break;
+ }
+  integral =(long)(numpd((2/3) * integral + error)); //Saturate integral Value
+  derivative = error - lastError;
 
-if (leftMotorSpeed > speed_regulation ) leftMotorSpeed = speed_regulation; 
-}else{
-  if(Flag==false){
-  cont++;
-  Flag=true;
-}
-  rightMotorSpeed = 255; //Full Speed in the Line 
-  leftMotorSpeed = 255;
-}
-if (rightMotorSpeed < 0) rightMotorSpeed = 0; 
+  motorSpeed = (double)((this->_Kp * error) + (this->_Ki * integral) + (this->_Kd * derivative));
 
-if (leftMotorSpeed < 0) leftMotorSpeed = 0; 
+  if (followerLine != true){
+    Flag=false;
+    speed_regulation=lastspeed_regulation;
 
-if(_maxTime != NO_LINE){
-if ( (error != lastError) && (millis() - Time > 0) ) {
-     Time=millis();
-} else{
- if ((millis() - Time >= _maxTime ) && (error == lastError) && (lastError == 0)  ){
-
-followerLine=true;
-return true;
-Time=millis();
   }else{
 
-    followerLine=false;
-   return false;
- }
-}
+    if(Flag==false){
+      cont++;
+      Flag=true;
+    }
+
+    speed_regulation=SpeedLine;
+  }
+
+  rightMotorSpeed = numpd((speed_regulation + motorSpeed));
+
+  leftMotorSpeed = numpd((speed_regulation - motorSpeed));
+
+  if (rightMotorSpeed > speed_regulation ) rightMotorSpeed = speed_regulation; 
+  if (leftMotorSpeed > speed_regulation ) leftMotorSpeed = speed_regulation; 
+  if (rightMotorSpeed < 0) rightMotorSpeed = 0; 
+  if (leftMotorSpeed < 0) leftMotorSpeed = 0; 
+
+  if(this->_maxTime != NO_LINE){
+    if ( (error != lastError) && (millis() - Time > 0) ) {
+      setOutput(13,0);
+      followerLine=false;
+      Time=millis();
+      return false;
+    } else{
+     if ((millis() - Time > this->_maxTime ) && (error == lastError) && (lastError == 0)  ){
+      setOutput(13,1);
+      if( duration == lastduration){
+        duration=_maxTime;
+      }
+      followerLine=true;
+      return true;
+      Time=millis();
+    }else{
+      duration= millis() - Time;
+      lastduration=duration;
+      followerLine=false;
+      return false;
+    }
+  }
 }else{
-  if( error== lastError && lastError==0){
+  if( error == lastError && lastError == 0){
+    setOutput(13,1);
+    duration= millis() - Time;
+    Flag2=false;
     return true;
+  }else{
+    if (Flag2==false)
+    {
+      cont++;
+      Flag2=true;
+    }
+
+    setOutput(13,0);
+    Time=millis();
+    return false;
   }
 }
+
 lastError = error;
 }
 int TB6612FNG::numpd(double x){  // ingresa un decimal con dos unidades despues del punto flotante para aproximarlo
@@ -268,3 +477,16 @@ int TB6612FNG::numpd(double x){  // ingresa un decimal con dos unidades despues 
     return (entero); 
   }
 }
+
+int freeMemory() {
+  int free_memory;
+
+  if((int)__brkval == 0)
+   free_memory = ((int)&free_memory) - ((int)&__bss_end);
+ else
+  free_memory = ((int)&free_memory) - ((int)__brkval);
+
+return free_memory;
+}
+
+
--- TB6612FNG.h
+++ TB6612FNG.h
@@ -17,8 +17,9 @@ http://forum.arduino.cc/index.php/topic,16612.0.html#4
 */
 #ifndef TB6612FNG_h
 #define TB6612FNG_h
-#include <arduino.h>
+#include <Arduino.h>
 #include <avr/io.h>
+#include <stdint.h>
 
 #define FORWARD true
 #define REVERSE false
@@ -27,50 +28,71 @@ http://forum.arduino.cc/index.php/topic,16612.0.html#4
 #define PWM_FREQUENCY_MIN 1024
 #define LIBRARY_VERSION 2.0.0
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+int freeMemory();
+
+#ifdef  __cplusplus
+}
+#endif
+
 class TB6612FNG
 {
  private:
 
-   void setPwmFrequency(byte pin, int divisor);
-   byte Pins[6];
-   byte mode;
+   void setPwmFrequency(uint8_t pin, int divisor);
+   int Pins[6];
+   uint8_t conec;
+   byte num;
    boolean rotating;
+   boolean fem1;
+   boolean fem2;
    boolean direction1;
    boolean direction2;
+   boolean followerLine;
+   boolean Flag;
+   boolean Flag2;
    int speed1;
    int speed2;
    int speed_regulation;
    int entero;
-   int decimal; 
+   int decimal;
+   int lastspeed_regulation;
+   int SpeedLine; 
+   int mode;
    double _Kp;
    double _Kd;
    double _Ki;
    double _setPoint;
+   double derivative;
    double lastError;
-   long integral;
    double ref;
-   unsigned long _maxTime;
-   boolean followerLine;
-   boolean Flag;
-
+   int32_t integral;
+   uint16_t _maxTime;
+   uint32_t lastduration;
+   double Fast_PWM_frequency;
  public:
 
-   TB6612FNG( byte PinsIn[], int freq_PWM);
+   TB6612FNG( int PinsIn[], int freq_PWM, uint8_t Conecction=0);
    ~TB6612FNG();
-   void Stop();
-   void setSpeed(int n_spd1, int n_spd2);
-   void ChangeSpeedRegulation(int n_spd_reg);
-   byte GetSpeedRegulation();
-   bool ControlPID(double input);
-   void setOutput(byte pin, boolean state);
-   void FollowerLinePID(double setPoint, double InputMax, double Kp, double Ki, double Kd,int maxTime=0);
-   int numpd(double x);
    int rightMotorSpeed;
    int leftMotorSpeed;
-   double motorSpeed;
    double error;
-   unsigned long Time;
    unsigned int cont;
+   double motorSpeed;
+   uint32_t Time;
+   uint32_t duration;
+   void ChangeSpeedRegulation(int n_spd_reg,int SpeedMax=0);
+   bool ControlPID(double input);
+   int numpd(double x);
+   void FollowerLinePID(double setPoint, double InputMax, double Kp, double Ki, double Kd,int maxTime=0);
+   uint8_t GetSpeedRegulation();
+   void setOutput(uint8_t pin, boolean state);
+   void setSpeed(int n_spd1, int n_spd2);
+   void Stop(int S_Time=0);
+   double GetFrequency();
+
 };
 
 #endif 
